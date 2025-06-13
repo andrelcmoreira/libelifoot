c5040dfc87149629c7ff30f82dab6ae64417067d
André L. C. Moreira <andrelcmoreira@disroot.org>
2025-06-12 22:40:01 -0300

    exporting 'Equipa' and 'UpdateEquipaListener' classes

--- libelifoot/__init__.py
+++ libelifoot/__init__.py
@@ -1,4 +1,8 @@
+from .entity.equipa import Equipa
+from .event.update_equipa_listener import UpdateEquipaListener
+
 from .libelifoot import bulk_update, update_equipa, view_equipa
 
 
-__all__ = ['bulk_update', 'update_equipa', 'view_equipa']
+__all__ = ['bulk_update', 'update_equipa', 'view_equipa', 'Equipa',
+           'UpdateEquipaListener']
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name='libelifoot',
     version='0.1.0',
-    description='',
+    description='Library to handle Elifoot equipas',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='André L. C. Moreira',
