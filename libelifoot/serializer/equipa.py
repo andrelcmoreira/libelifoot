b1de500e0f329e8d1b6629c196eba2027c88403d
André L. C. Moreira <andrelcmoreira@disroot.org>
2025-06-20 00:37:56 -0300

    adding Equipa serializer

---
+++ libelifoot/serializer/equipa.py
@@ -0,0 +1,26 @@
+from libelifoot.entity.equipa import Equipa
+from libelifoot.serializer.base_serializer import BaseSerializer
+from libelifoot.serializer.coach import CoachSerializer
+from libelifoot.serializer.player import PlayerSerializer
+from libelifoot.util.crypto import encrypt
+
+
+class EquipaSerializer(BaseSerializer):
+
+    @staticmethod
+    def serialize(obj: Equipa) -> bytearray:
+        equipa = bytearray(b'EFa' + b'\x00' * 47)
+
+        equipa += encrypt(obj.ext_name)
+        equipa += encrypt(obj.short_name)
+        equipa += bytearray(obj.colors.background + b'\x00')
+        equipa += bytearray(obj.colors.text) # ???
+        equipa += encrypt(obj.country)
+        equipa += bytearray(obj.level.to_bytes())
+        equipa += bytearray(len(obj.players).to_bytes())
+        for p in obj.players:
+            equipa += PlayerSerializer.serialize(p)
+
+        equipa += CoachSerializer.serialize(obj.coach)
+
+        return equipa
