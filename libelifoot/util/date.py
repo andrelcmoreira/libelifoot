f020bdb534941f6d99b2b2e741c631d993f964ad
André L. C. Moreira <andrelcmoreira@disroot.org>
2025-08-25 23:25:21 -0300

    adding util.date module

---
+++ libelifoot/util/date.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+from time import localtime
+
+
+_DAYS_IN_YEAR = 365
+
+
+def _get_work_days_in_session(start: str, end: str) -> int:
+    date_start = datetime.strptime(start, '%d/%m/%Y')
+
+    if end:
+        date_end = datetime.strptime(end, '%d/%m/%Y')
+    else:
+        lt = localtime()
+        date_end = datetime(year=lt.tm_year, month=lt.tm_mon, day=lt.tm_mday)
+
+    diff = date_end - date_start
+
+    return diff.days
+
+
+def _get_work_days_in_start_session(end: str, season: int) -> int:
+    date_start = datetime.strptime(f"01/01/{season}", '%d/%m/%Y')
+    date_end = datetime.strptime(end, '%d/%m/%Y')
+    diff = date_end - date_start
+
+    return diff.days
+
+
+def _get_work_days_in_end_session(start: str, season: int) -> int:
+    date_start = datetime.strptime(start, '%d/%m/%Y')
+    date_end = datetime.strptime(f"31/12/{season}", '%d/%m/%Y')
+    diff = date_end - date_start
+
+    return diff.days
+
+
+def get_work_days_in_season(season: int, start: str, end: str) -> int:
+    start_season_year = int(start.split('/')[2])
+    end_season_year = int(end.split('/')[2]) if end else localtime().tm_year
+
+    if start_season_year == season == end_season_year:
+        return _get_work_days_in_session(start, end)
+    if (season == start_season_year) and (season != end_season_year):
+        return _get_work_days_in_start_session(end, season)
+    if season > start_season_year and season == end_season_year:
+        return _get_work_days_in_end_session(start, season)
+    if end_season_year > season > start_season_year:
+        return _DAYS_IN_YEAR
+
+    return 0
