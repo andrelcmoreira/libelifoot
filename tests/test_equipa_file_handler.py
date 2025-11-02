5be51635784f6e9c8c6e1bc3e943b405e1c612a3
André L. C. Moreira <andrelcmoreira@disroot.org>
2025-11-02 15:50:22 -0300

    adding more unit tests to file.equipa module

--- tests/test_equipa_file_handler.py
+++ tests/test_equipa_file_handler.py
@@ -1,4 +1,4 @@
-from unittest.mock import patch
+from unittest import mock
 
 from fixtures import mock_equipa
 
@@ -6,10 +6,25 @@ from libelifoot.file.equipa import EquipaFileHandler
 
 
 def test_read_equipa(mock_equipa):
-    with patch(
+    with mock.patch(
         'libelifoot.parser.equipa.EquipaParser.parse',
         return_value=mock_equipa
     ) as mock_parse:
         assert EquipaFileHandler.read('FORTALEZA.EFT') == mock_equipa
 
         mock_parse.assert_called_once()
+
+
+@mock.patch('libelifoot.serializer.equipa.EquipaSerializer.serialize')
+def test_write_equipa(mock_serialize, mock_equipa):
+    file_path = 'FORTALEZA.EFT'
+    serialized_equipa = b'\x00\x01\x02\x03\x04\x05'
+
+    mock_serialize.return_value = serialized_equipa
+
+    with mock.patch('builtins.open', mock.mock_open()) as mock_file:
+        EquipaFileHandler.write(file_path, mock_equipa)
+
+        mock_file.assert_called_once_with(file_path, 'wb')
+        mock_serialize.assert_called_once_with(mock_equipa)
+        mock_file().write.assert_called_once_with(serialized_equipa)
