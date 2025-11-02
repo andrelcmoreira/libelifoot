0837af24c9710f4acc22fc203498e6f5265100b5
André L. C. Moreira <andrelcmoreira@disroot.org>
2025-11-02 15:30:02 -0300

    adding more unit tests to util.date module

--- tests/test_date.py
+++ tests/test_date.py
@@ -1,3 +1,6 @@
+from unittest.mock import patch
+from time import struct_time
+
 from libelifoot.util.date import get_work_days_in_season
 
 
@@ -23,3 +26,11 @@ def test_get_work_days_in_season_middle_year():
 
 def test_get_work_days_in_season_with_no_working_days():
     assert get_work_days_in_season(2019, '15/03/2020', '20/06/2022') == 0
+
+
+def test_get_work_days_in_season_with_no_end():
+    with patch('time.localtime') as mock_localtime:
+        t = struct_time((2025, 11, 4, 0, 0, 0, 0, 0, 0))
+        mock_localtime.return_value = t
+
+        assert get_work_days_in_season(2025, '15/03/2025', '') == 234
