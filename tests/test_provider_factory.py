72f2c7ef5312ac8e87d778e22fb7612765bc9562
André L. C. Moreira <andrelcmoreira@disroot.org>
2025-11-14 20:59:14 -0300

    adding unit tests for provider.factory module

---
+++ tests/test_provider_factory.py
@@ -0,0 +1,31 @@
+from pytest import raises
+
+from libelifoot.error.unknown_provider import UnknownProvider
+from libelifoot.provider import espn
+from libelifoot.provider import factory
+from libelifoot.provider import transfermarkt
+
+
+def test_create_roster_provider_with_espn():
+    provider = factory.create_roster_provider('espn')
+
+    assert isinstance(provider, espn.RosterProvider)
+
+
+def test_create_roster_provider_with_transfermarkt():
+    provider = factory.create_roster_provider('transfermarkt')
+
+    assert isinstance(provider, transfermarkt.RosterProvider)
+
+
+def test_create_roster_provider_with_invalid_entry():
+    provider = 'invalid_provider'
+
+    with raises(UnknownProvider, match=f"unknown provider '{provider}'!"):
+        factory.create_roster_provider(provider)
+
+
+def test_create_coach_provider():
+    provider = factory.create_coach_provider()
+
+    assert isinstance(provider, transfermarkt.CoachProvider)
