fe74f1fc4ae36aeedbd9847832d9537015760fc4
André L. C. Moreira <andrelcmoreira@proton.me>
2026-01-10 14:07:54 -0300

    adding docstrings to update_equipa_listener module

--- libelifoot/event/update_equipa_listener.py
+++ libelifoot/event/update_equipa_listener.py
@@ -7,8 +7,17 @@ class UpdateEquipaListener(ABC): # pragma: no cover
 
     @abstractmethod
     def on_update_equipa(self, equipa_name: str, equipa_data: Equipa) -> None:
-        pass
+        """
+        Invoked when an equipa is successfully updated.
+
+        :equipa_name: The name of the equipa.
+        :equipa_data: The updated equipa data.
+        """
 
     @abstractmethod
     def on_update_equipa_error(self, error: str) -> None:
-        pass
+        """
+        Invoked when there is an error updating an equipa.
+
+        :error: The error message.
+        """
