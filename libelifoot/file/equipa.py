14760db236a7faa1ed3776e99507add9ddf2ca20
André L. C. Moreira <andrelcmoreira@proton.me>
2026-02-11 21:41:01 -0300

    lib: documenting libelifoot.file.equipa module

--- libelifoot/file/equipa.py
+++ libelifoot/file/equipa.py
@@ -7,6 +7,12 @@ class EquipaFileHandler:
 
     @staticmethod
     def write(file_name: str, equipa: Equipa) -> None:
+        """
+        Write an equipa to an EFT file.
+
+        :file_name: The file name to write the equipa to.
+        :equipa: The equipa to write.
+        """
         with open(file_name, 'wb') as f:
             data = EquipaSerializer.serialize(equipa)
 
@@ -15,6 +21,12 @@ class EquipaFileHandler:
 
     @staticmethod
     def read(file_name: str) -> Equipa:
+        """
+        Read an equipa from an EFT file.
+
+        :file_name: The file name to read the equipa from.
+        :return: The equipa read from the file.
+        """
         ep = EquipaParser(file_name)
 
         return ep.parse()
