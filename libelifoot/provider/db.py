3fbc6e4517608574b5cbf10f78b797e7211064b0
André L. C. Moreira <andrelcmoreira@proton.me>
2026-03-22 12:07:22 -0300

    lib: renaming provider.config to provider.data

--- libelifoot/provider/db.py
+++ libelifoot/provider/db.py
@@ -1,14 +1,14 @@
+import json
 import os
-from json import load
-from pathlib import Path
+import pathlib
 
 
-_CONFIG_PATH = Path(__file__).parent / 'config'
+_DATA_PATH = pathlib.Path(__file__).parent / 'data'
 
 
 def get_team_id(equipa_file: str, provider: str) -> str:
-    with open(f'{_CONFIG_PATH}/{provider}.json', encoding='utf-8') as f:
-        data = load(f)
+    with open(f'{_DATA_PATH}/{provider}.json', encoding='utf-8') as f:
+        data = json.load(f)
 
         for entry in data:
             if entry['file'] == equipa_file:
@@ -18,11 +18,11 @@ def get_team_id(equipa_file: str, provider: str) -> str:
 
 
 def get_teams(provider: str) -> list[dict]:
-    with open(f'{_CONFIG_PATH}/{provider}.json', encoding='utf-8') as f:
-        data = load(f)
+    with open(f'{_DATA_PATH}/{provider}.json', encoding='utf-8') as f:
+        data = json.load(f)
 
         return data
 
 
 def get_available_providers() -> list[str]:
-    return [i.split('.')[0] for i in os.listdir(_CONFIG_PATH)]
+    return [i.split('.')[0] for i in os.listdir(_DATA_PATH)]
