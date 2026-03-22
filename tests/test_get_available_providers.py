6090c729fd7d3ce9ba37c917bd73b93bab5d49b4
André L. C. Moreira <andrelcmoreira@proton.me>
2026-03-21 22:11:43 -0300

    lib: adding unit tests for get_available_providers command

---
+++ tests/test_get_available_providers.py
@@ -0,0 +1,16 @@
+from unittest.mock import patch
+
+from libelifoot.api import get_available_providers
+
+
+def test_get_equipa_data():
+    fake_providers = ['provider-1', 'provider-2', 'provider-3']
+
+    with patch(
+        'libelifoot.provider.db.get_available_providers',
+        return_value=fake_providers
+    ) as mock_get:
+        cmd = get_available_providers.Cmd()
+
+        assert cmd.run() == fake_providers
+        mock_get.assert_called_once_with()
