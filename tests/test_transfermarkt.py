953b18d0f6d76fb9cd79b43d88489fd6dd75b935
André L. C. Moreira <andrelcmoreira@proton.me>
2026-09-06 19:37:21 -0300

    lib: fixing unit tests on test_transfermarkt suite

--- tests/test_transfermarkt.py
+++ tests/test_transfermarkt.py
@@ -4,6 +4,7 @@ from unittest.mock import MagicMock
 
 from fixtures import mock_players
 
+from libelifoot.domain.entity.equipa_db_entry import EquipaDbEntry
 from libelifoot.domain.error.equipa_data_not_available import EquipaDataNotAvailable
 from libelifoot.domain.error.equipa_not_provided import EquipaNotProvided
 from libelifoot.infrastructure.provider.impl.transfermarkt import (
@@ -12,29 +13,30 @@ from libelifoot.infrastructure.provider.impl.transfermarkt import (
 )
 
 
-ROSTER_PROV = RosterProvider(MagicMock())
 BASE_URL = 'https://www.transfermarkt.com.br'
 
 
 def test_assemble_roster_uri_with_season_year():
+    roster_prov = RosterProvider(MagicMock())
     team_id = 'manchester-united/{}/verein/985'
     expected_id = team_id.format('startseite')
     season = 2022
 
-    uri = ROSTER_PROV.assemble_uri(team_id, season)
+    uri = roster_prov.assemble_uri(team_id, season)
     assert f'{BASE_URL}/{expected_id}/saison_id/{season}' == uri
 
 
 def test_assemble_roster_uri_with_no_season_year():
+    roster_prov = RosterProvider(MagicMock())
     team_id = 'manchester-united/{}/verein/985'
     expected_id = team_id.format('startseite')
 
-    uri = ROSTER_PROV.assemble_uri(team_id, 0)
+    uri = roster_prov.assemble_uri(team_id, 0)
     assert  f'{BASE_URL}/{expected_id}' == uri
 
 
 def test_assemble_coach_uri():
-    coach_prov = CoachProvider()
+    coach_prov = CoachProvider(MagicMock())
     team_id = 'manchester-united/{}/verein/985'
     expected_id = team_id.format('mitarbeiterhistorie')
     season = 2022
@@ -44,104 +46,127 @@ def test_assemble_coach_uri():
 
 
 def test_get_coach_with_provided_equipa():
-    team_id = 'manchester-united/mitarbeiterhistorie/verein/985'
     coach_name = 'Ralf Rangnick'
+    equipa_entry = EquipaDbEntry(
+        id='manchester-united/mitarbeiterhistorie/verein/985',
+        file='MANCITY.EFT'
+    )
     season = 2022
-    coach_prov = CoachProvider()
+    repo_mock = MagicMock()
+    coach_prov = CoachProvider(repo_mock)
 
-    with (
-        mock.patch(
-            'libelifoot.provider.db.get_team_id',
-            return_value=team_id
-        ) as mock_get_team_id,
-        mock.patch.object(
-            CoachProvider,
-            '_fetch_coach_data',
-            return_value=coach_name
-        ) as mock_fetch_coach_data,
-    ):
-        name = coach_prov.get_coach(team_id, season)
+    repo_mock.get_team.return_value = equipa_entry
+
+    with mock.patch.object(
+        CoachProvider,
+        '_fetch_data',
+        return_value=coach_name
+    ) as mock_fetch_data:
+        name = coach_prov.get_coach(equipa_entry.file, season)
 
         assert name == coach_name
 
-        mock_get_team_id.assert_called_once_with(team_id, coach_prov.name)
-        mock_fetch_coach_data.assert_called_once_with(team_id, season)
+        repo_mock.get_team.assert_called_once_with(
+            equipa_entry.file,
+            coach_prov.name
+        )
+        mock_fetch_data.assert_called_once_with(equipa_entry.id, season)
 
 
 def test_get_coach_with_no_provided_equipa():
-    team_id = 'FORTALEZA.EFT'
+    equipa_entry = EquipaDbEntry(
+        id='manchester-united/mitarbeiterhistorie/verein/985',
+        file='MANCITY.EFT'
+    )
     season = 2022
-    coach_prov = CoachProvider()
+    repo_mock = MagicMock()
+    coach_prov = CoachProvider(repo_mock)
 
-    with mock.patch(
-        'libelifoot.provider.db.get_team_id',
-        return_value=''
-    ) as mock_get_team_id:
-        with raises(EquipaNotProvided):
-            coach_prov.get_coach(team_id, season)
+    repo_mock.get_team.return_value = None
+    with raises(EquipaNotProvided):
+        coach_prov.get_coach(equipa_entry.file, season)
 
-        mock_get_team_id.assert_called_once_with(team_id, coach_prov.name)
+    repo_mock.get_team.assert_called_once_with(
+        equipa_entry.file,
+        coach_prov.name
+    )
 
 
 def test_get_players_with_no_provided_equipa():
-    team_id = 'FORTALEZA.EFT'
+    equipa_entry = EquipaDbEntry(
+        id='manchester-united/mitarbeiterhistorie/verein/985',
+        file='MANCITY.EFT'
+    )
     season = 2022
+    repo_mock = MagicMock()
+    roster_prov = RosterProvider(repo_mock)
 
-    with mock.patch(
-        'libelifoot.provider.db.get_team_id',
-        return_value=''
-    ) as mock_get_team_id:
-        with raises(EquipaNotProvided):
-            ROSTER_PROV.get_players(team_id, season)
+    repo_mock.get_team.return_value = None
 
-        mock_get_team_id.assert_called_once_with(team_id, ROSTER_PROV.name)
+    with raises(EquipaNotProvided):
+        roster_prov.get_players(equipa_entry.file, season)
+
+    repo_mock.get_team.assert_called_once_with(
+        equipa_entry.file,
+        roster_prov.name
+    )
 
 
 def test_get_players_with_no_data_available():
-    team_id = 'manchester-united/mitarbeiterhistorie/verein/985'
+    equipa_entry = EquipaDbEntry(
+        id='manchester-united/mitarbeiterhistorie/verein/985',
+        file='MANCITY.EFT'
+    )
     season = 2022
+    repo_mock = MagicMock()
+    roster_prov = RosterProvider(repo_mock)
 
-    with (
-        mock.patch(
-            'libelifoot.provider.db.get_team_id',
-            return_value=team_id
-        ) as mock_get_team_id,
-        mock.patch.object(
-            ROSTER_PROV,
-            '_fetch_team_data',
-            return_value=[]
-        ) as mock_fetch_team_data,
-    ):
+    repo_mock.get_team.return_value = equipa_entry
+
+    with mock.patch.object(
+        roster_prov,
+        '_fetch_data',
+        return_value=[]
+    ) as mock_fetch_team_data:
         with raises(EquipaDataNotAvailable):
-            ROSTER_PROV.get_players(team_id, season)
+            roster_prov.get_players(equipa_entry.file, season)
 
-        mock_get_team_id.assert_called_once_with(team_id, ROSTER_PROV.name)
-        mock_fetch_team_data.assert_called_once_with(team_id, season)
+        repo_mock.get_team.assert_called_once_with(
+            equipa_entry.file,
+            roster_prov.name
+        )
+        mock_fetch_team_data.assert_called_once_with(equipa_entry.id, season)
 
 
 def test_get_players_with_data_available(mock_players):
-    team_id = 'manchester-united/mitarbeiterhistorie/verein/985'
+    equipa_entry = EquipaDbEntry(
+        id='manchester-united/mitarbeiterhistorie/verein/985',
+        file='MANCITY.EFT'
+    )
     season = 2022
+    repo_mock = MagicMock()
+    roster_prov = RosterProvider(repo_mock)
+
+    repo_mock.get_team.return_value = equipa_entry
 
     with (
-        mock.patch(
-            'libelifoot.provider.db.get_team_id',
-            return_value=team_id
-        ) as mock_get_team_id,
         mock.patch.object(
-            ROSTER_PROV,
-            '_fetch_team_data',
+            roster_prov,
+            '_fetch_data',
             return_value=mock_players
-        ) as mock_fetch_team_data,
+        ) as mock_fetch_data,
         mock.patch(
             'libelifoot.infrastructure.provider.impl.transfermarkt.RosterProvider.select_players',
             return_value=mock_players
         ) as mock_select_players,
     ):
-        players = ROSTER_PROV.get_players(team_id, season)
+        players = roster_prov.get_players(equipa_entry.file, season)
 
         assert players == mock_players
 
-        mock_get_team_id.assert_called_once_with(team_id, ROSTER_PROV.name)
-        mock_fetch_team_data.assert_called_once_with(team_id, season)
+        repo_mock.get_team.assert_called_once_with(
+            equipa_entry.file,
+            roster_prov.name
+        )
+        mock_fetch_data.assert_called_once_with(equipa_entry.id, season)
         mock_select_players.assert_called_once_with(mock_players)
