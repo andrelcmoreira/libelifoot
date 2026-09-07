a2afd7a8409f1372e7955f0349788fe2ad2bae71
André L. C. Moreira <andrelcmoreira@proton.me>
2026-09-07 14:33:19 -0300

    lib: fixing unit tests of test_provider_factory suite

--- tests/test_provider_factory.py
+++ tests/test_provider_factory.py
@@ -1,4 +1,5 @@
 from pytest import raises
+from unittest.mock import MagicMock
 
 from libelifoot.domain.error.unknown_provider import UnknownProvider
 from libelifoot.infrastructure.provider import factory
@@ -7,13 +8,13 @@ from libelifoot.infrastructure.provider.impl import transfermarkt
 
 
 def test_create_roster_provider_with_espn():
-    provider = factory.create_roster_provider('espn')
+    provider = factory.create_roster_provider('espn', MagicMock())
 
     assert isinstance(provider, espn.RosterProvider)
 
 
 def test_create_roster_provider_with_transfermarkt():
-    provider = factory.create_roster_provider('transfermarkt')
+    provider = factory.create_roster_provider('transfermarkt', MagicMock())
 
     assert isinstance(provider, transfermarkt.RosterProvider)
 
@@ -22,10 +23,10 @@ def test_create_roster_provider_with_invalid_entry():
     provider = 'invalid_provider'
 
     with raises(UnknownProvider, match=f"Unknown provider '{provider}'!"):
-        factory.create_roster_provider(provider)
+        factory.create_roster_provider(provider, MagicMock())
 
 
 def test_create_coach_provider():
-    provider = factory.create_coach_provider()
+    provider = factory.create_coach_provider(MagicMock())
 
     assert isinstance(provider, transfermarkt.CoachProvider)
